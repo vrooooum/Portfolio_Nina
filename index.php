a49aa650dc5e889b42ddc5992edfc16b041c87ae
vrooooum <70584431+vrooooum@users.noreply.github.com>
2020-09-01 15:11:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<title>Nina Antkowiak - Graphiste indépendante</title>
+
+	<link rel="icon" href="./img/favicon.ico"/>
+
+	<!-- C S S -->
+	<link rel="stylesheet" href="style.css" type="text/css">
+
+
+	<!-- F O N T S -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,300,400,500,700,800&display=swap" rel="stylesheet">
+
+	<!-- Font awersome-->
+	<script src="https://kit.fontawesome.com/e0302df81e.js" crossorigin="anonymous"></script>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+	<!-- Javascript -->
+	<script type="text/javascript" src="animate.js"></script>
+
+	<!-- Fade css -->
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+
+
+<!--     
+animation fade
+<script>
+AOS.init();
+</script> -->
+
+
+</head>
+
+<body>
+
+
+<!-- ACCUEIL -->
+	<section id="home">
+
+<!-- NAVIGATION -->
+		<nav class="navbar fixed-top navbar-expand-lg navbar-light">
+			<a class="navbar-brand" href="#"><img src="./img/logonavbar.svg" alt="logo-Na" id="logo"></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="#skills">Compétences<span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#projects">Projets</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#services">Services</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#about">A Propos</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contact">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+
+
+<div class="d-flex justify-content-center">
+	<div id="title">
+		<div id="nina">Nina</div>
+		<div class="stroke">Antkowiak</div>
+		<div id="slog" style="font-weight: lighter;" >Prête à vous accompagner pour vos projets web</div>
+
+<!-- ANIMATION SOURIS -->
+		<div class="scroll-downs">
+			<a onclick="scroll(0, 1000);">
+				<div class="mousey">
+					<div class="scroller"></div>
+				</div>
+			</a>
+		</div> <!-- FIN ANIMATION SOURIS -->
+	</div> 
+</div>
+</section>
+
+<!-- COMPÉTENCES -->
+<section id="skills">
+	<div class="container">
+
+
+		<h1 class="title">Compétences & expériences</h1>
+		<div class="row">
+			<div class="col text">
+
+				<div class="subtitle">Etudes</div>
+				<ul class="text">
+					<li class="line">LICENCE : MÉTIERS DU NUMÉRIQUE</li>
+					<li class="line">DUT : MÉTIERS DU MULTIMÉDIA & DE L'INTERNET</li>
+					<li class="line">BAC : SCIENCE & TECHNOLOGIES DU DESIGN & DES ARTS APPLIQUÉES</li>
+				</ul>
+			</div>
+
+			<div class="col text">
+				<div class="subtitle">Expérience</div> 
+				<ul class="text">
+					<li class="line">1 an en entreprise en alternance</li>
+					<li class="line">Autoentrepreuneuse depuis janvier 2020</li>
+					<li class="line">Passionée depuis toujours</li>
+				</ul>
+			</div>
+
+			<ul class="col text">
+				<li class="subtitle">Compétences</li> 
+				<li class="text">
+				<li class="line">Créative, Curieuse, Esprit d'équipe, Capacité d'adaptation, Autonome...</li>
+				<li class="line">Et bien plus encore</li>
+			</ul>
+			</div>
+		</div>
+
+			<div class="d-flex justify-content-center">
+				<a href="#contact"><button class="bouton hvr-sink" type="button">On travaille ensemble ?</button></a>
+			</div>
+	</section>
+
+<!-- PROJETS -->
+	<section id="projects">
+		<div class="container">
+
+			<h1 class="title">Mes derniers projets</h1>
+			<div class="row">
+
+				<div class="col hover">
+					<img class="grid-item" src="./img/guardnity.png" alt="Graphisme">
+					<div class="middle">
+						<div class="text-bold">Guardnity</div>
+						<div class="desc">Site vitrine</div>
+					</div>
+				</div>
+
+				<div class="col hover">
+					<img class="grid-item" src="./img/cnac.png" alt="Graphisme">
+					<div class="middle">
+						<div class="text-bold">CNAC</div>
+						<div class="desc">Design site e-commerce</div>
+					</div>
+
+				</div>
+				<div class="col hover"><img class="grid-item" src="./img/reunion.png" alt="Graphisme">
+					<div class="middle">
+						<div class="text-bold">La réunion</div>
+						<div class="desc">Site vitrine</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col hover"><img class="grid-item" src="./img/modestfashion.png" alt="Graphisme">
+					<div class="middle">
+						<div class="text-bold">Modest Fashion France</div>
+						<div class="desc">Identité visiuelle</div>
+					</div>
+				</div>
+
+				<div class="col hover"><img class="grid-item" src="./img/hir.png" alt="Graphisme">
+					<div class="middle">
+						<div class="text-bold">HIR</div>
+						<div class="desc">Application</div>
+					</div>
+				</div>
+
+				<div class="col hover"><img class="grid-item" src="./img/greevent.png" alt="Graphisme">
+					<div class="middle">
+						<div class="text-bold">Greevent</div>
+						<div class="desc">WordPress</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="d-flex justify-content-center">
+			<a href="./projects.html"><button class="bouton hvr-sink">Voir plus +</button></a>
+		</div>
+	</section>
+
+<!-- SERVICES -->
+	<section id="services">
+		<div class="container">
+
+			<h1 class="title">A votre service !</h1>
+			<div class="service">
+				<div class="d-flex flex-row justify-content-center">
+
+					<div class="col">
+						<img src="./img/idea.svg" alt="Création" class="center">
+						<div class="subtitle">Création</div>
+						<ul>
+							<li>Création</li>
+							<li>Illustrations</li>
+							<li>Montages</li>
+							<li>Retouches</li>
+							<li>Visuels réseaux sociaux</li>
+						</ul>
+					</div>
+
+					<div class="col">
+						<img src="./img/pentool.svg" alt="Graphisme" class="center">
+						<div class="subtitle">Graphisme</div>
+						<ul>
+							<li>Graphisme</li>
+							<li>Logos</li>
+							<li>Charte graphique</li>
+							<li>Mise en page</li>
+							<li>Flyers</li>
+							<li>Brochures</li>
+							<li>Kakémono</li>
+						</ul>
+					</div>
+					<div class="col"><img src="./img/interfaces.svg" alt="Interfaces" class="center">
+						<div class="subtitle">Web</div>
+						<ul>
+							<li>Interface</li>
+							<li>Design d'interface</li>
+							<li>Réflexion graphique</li>
+							<li>Projet en design thinking</li>
+							<li>Maquette Xd/Sketch</li>
+							<li>Site web Wordpress</li>
+						</ul>
+					</div>
+				</div>
+
+
+				<div class="d-flex flex-row justify-content-center">
+					<a href="https://www.malt.fr/profile/ninaantkowiak" target="_blank"><button class="bouton hvr-sink" type="button">Demander un devis</button></a>
+				</div>
+
+
+			</div>
+		</section>
+
+
+<!-- A PROPOS -->
+		<section id="about">
+
+
+			<div class="container">
+				<div class="content-about">
+				
+					<div class="d-flex justify-content-center">
+						
+						<img src="./img/photo.png" id="profil"></div>
+					</div>
+
+						<div class="d-flex justify-content-center">
+							<div class="cards">
+								<h1 class="d-flex justify-content-center titlecard">
+									<span class="hightlight">Qui suis-je ?</span>
+								</h1>
+								<p>
+									Enchantée ! Je suis Nina Antkowiak. 
+									<br/>
+									Depuis toujours passionnée par l'art, le design et le graphisme, j'ai décidée de devenir graphiste orienté web et conception d'interface utilisateur.
+									<br/>
+									L'identité graphique deviens de plus en plus importante dans notre société à l'ère du numérique. Beaucoup ont besoins de la leur pour être plus visible sur les plateforme web.
+									<br/>
+									Pour moi, il est aussi important de se rappeler que ces nouvelles technologies sont là pour améliorer notre vie et nos expériences. Une bonne interface doit donc être réfléchie et efficace en remettant l'humain au centre de celle-ci.
+								</p>
+
+								<div class="d-flex justify-content-center">
+										<button class="bouton hvr-sink" target="_blank" onclick="window.open('./ANTKOWIAK_Nina_CV.pdf')" download="Antkowiak-Nina-CV" type="button">Télécharger mon CV</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</section>
+
+<!-- CONTACT -->
+			<section id="contact">
+				<div class="container">
+
+					<div class="d-flex justify-content-center">
+						<div class="cards">
+							<h1 class="d-flex justify-content-center titlecard"><span class="hightlight">Une idée ? Un projet ?</span></h1>
+							<p style="text-align: center;">Écrivez-moi à <a target="_blank" href="mailto:ninaantkowiak.pro@gmail.com" style="text-decoration: none; color:black;">ninaantkowiak.pro@gmail.com</a><br/>
+							ou remplissez le formulaire ci-dessous</p>
+							<form method="post">
+								<div class="form-group">
+									<label for="email">Adresse email *</label>
+									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="mail" required>
+									<small id="emailHelp" class="form-text text-muted">Cet email ne sera jamais partager avec quelqu'un d'autre. Ça sera notre petit secret ;)</small>
+								</div>
+								<div class="form-group">
+									<label for="exampleInputObject">Objet *</label>
+									<input type="object" class="form-control" id="exampleInputPassword1" name="objet" required>
+								</div>
+								<div class="form-group">
+									<label for="exampleFormControlTextarea1" name="message">Message *</label>
+									<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="message" required></textarea>
+								</div>
+
+								<div class="d-flex justify-content-center">
+									<button type="submit" class="bouton hvr-sink">Envoyer</button>
+								</div>
+							</form>
+								
+								<?php
+									if (isset($_POST['message'])) {
+							     		$position_arobase = strpos($_POST['mail'], '@');
+							     		if ($position_arobase == false)
+							         		echo '<p>Votre email n\'est pas valide.</p>';
+	                             		else {
+	                                   		$retour = mail('ninaantkowiak.pro@gmail.com', $_POST['objet'], $_POST['message'], 'From: ' . $_POST['mail']);
+	                                   		if($retour)
+	                                       		echo '<p>Votre message a été envoyé.</p>';
+	                                   		else
+	                                       		echo '<p>Erreur.</p>';
+	                             		}
+						    		}
+								?>
+								
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+			<footer>
+				<div class="social d-flex justify-content-center">
+					<a target="_blank" href="https://www.linkedin.com/in/nina-antkowiak/"><i class="fab fa-linkedin"></i></a>
+					<a target="_blank" href="https://www.instagram.com/nina.antko/"><i class="fab fa-instagram"></i></a>
+					<a target="_blank" href="https://dribbble.com/ninantko"><i class="fab fa-dribbble"></i></a>
+					<a target="_blank" href="https://www.behance.net/ninaantkowiak"><i class="fab fa-behance"></i></i></a>
+					<a target="_blank" href="mailto:ninaantkowiak.pro@gmail.com"><i class="fas fa-paper-plane"></i></a>
+				</div>
+
+			</div>
+			<p class="d-flex justify-content-center" style="font-size: 9px; color: #9E9E9E; margin: 0px;">© Nina Antkowiak - 2020 - codé avec ❤️</p>
+		</footer>
+
+		<!-- jquery -->
+		<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+		<!-- lien animations fade https://michalsnik.github.io/aos/-->
+		<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+		<!-- <script src="active-menu-link.js"></script> -->
+	  	<script>AOS.init();</script>
+	</body>
+	</html>
